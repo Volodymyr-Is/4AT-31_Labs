a5507f1de0066c168511e08ee3a4403434cb64bd
Volodymyr-Is <isaienkovova05@gmail.com>
2025-09-05 20:01:30 +0300

    Lab 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 3;
+
+        var sum = a + b;
+        var sub = a - b;
+        var mult = a * b;
+        var div = a / b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Sub: " + sub);
+        System.out.println("Mult: " + mult);
+        System.out.println("Div: " + div);
+    }
+}
