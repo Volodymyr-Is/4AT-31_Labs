918169ccff1fc5fc184898f69b8cb4256ff21de8
Volodymyr-Is <isaienkovova05@gmail.com>
2025-09-08 09:13:26 +0300

    Lab 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+//        V 1: Basic Arithmetic Operations
+//        Objective: To understand and perform basic arithmetic operations in Java using primitive types.
+//        Instructions:
+//        Declare two int variables.
+//        Assign values to the variables and perform basic arithmetic operations (e.g., addition, subtraction, multiplication, division).
+//        Print the results of each operation to the console.
+
+
         int a = 6;
         int b = 3;
 
